0a21a07b0458f10789425f1521814d91ff550c39
Mert Yılmaz <b202102029@subu.edu.tr>
2021-08-11 22:09:55 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,21 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+
+            foreach (var product in productManager.GettAll())
+            {
+                Console.WriteLine(product.ProductName);
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
