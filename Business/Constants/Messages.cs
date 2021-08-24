ff80f00c677ca0d6bad87fb4b2c4d495d8d75bb1
Mert Yılmaz <b202102029@subu.edu.tr>
2021-08-25 00:08:53 +0300

    .

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -9,7 +9,7 @@ namespace Business.Constants
     {
         public static string ProductAdded = "Ürün eklendi";
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
-        internal static string MaintenanceTime = "Sistem bakımda";
-        internal static string ProductListed = "Ürünler Listelendi";
+        public static string MaintenanceTime = "Sistem bakımda";
+        public static string ProductListed = "Ürünler Listelendi";
     }
 }
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -33,8 +33,8 @@ namespace WebAPI
             //Autofact, Ninject, CastleWindsor, StructerMap, LightInject, DryInJect --> Container
             //AOP
             services.AddControllers();
-            services.AddSingleton<IProductService,ProductManager>();
-            services.AddSingleton<IProductDal, EfProductDal>();
+            //services.AddSingleton<IProductService,ProductManager>();
+            //services.AddSingleton<IProductDal, EfProductDal>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
